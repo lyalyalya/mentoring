5094c6fc8d7e94d6202b596eb47f06dd507a567b
olga.dmitrova <olga.dmitrova@rtmis.ru>
2020-04-15 05:45:52 +0300

    Task 2&3: jdk tools and MAT for heap dump analysis

---
+++ src/main/java/task3/Demo.java
@@ -0,0 +1,31 @@
+package task3;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Demo {
+
+    private static String hello = "Hello ";
+    private static String world = "world !";
+    private static String result = hello + world;
+
+    @SneakyThrows
+    public static void concatWords() {
+        while (true) {
+            hello = result + hello;
+            world = hello + world;
+            result = world + result;
+            Thread.sleep(5000);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            concatWords();
+        } catch (OutOfMemoryError e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+}
