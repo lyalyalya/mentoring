5fa3701183e1acff713ba37ac0e1af4eb3265a78
olga.dmitrova <olga.dmitrova@rtmis.ru>
2020-04-15 16:18:19 +0300

    Добавлен вывод итерации

--- src/main/java/task5/Demo.java
+++ src/main/java/task5/Demo.java
@@ -10,12 +10,14 @@ public class Demo {
 
     @SneakyThrows
     public static void main(String[] args) {
+        int iteration = 0;
         while (true) {
             for (int i = 0; i < 100; i++) {
                 for (int j = 0; j < 1000; j++) {
                     POOL.add(new String("j = " + j));
                 }
             }
+            System.out.println("Iteration " + iteration++);
             Thread.sleep(100);
         }
     }
